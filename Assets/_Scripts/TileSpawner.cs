18683e7b0337473d40d406914b65b0eeab9dc589
RobyDoge <robert-ionut.popa@student.unitbv.ro>
2024-12-05 19:11:58 +0200

    Infinte Tile Generation

--- Assets/_Scripts/TileSpawner.cs
+++ Assets/_Scripts/TileSpawner.cs
@@ -39,17 +39,61 @@ public class TileSpawner : MonoBehaviour
 
         for (int i = 0; i < _tileStartCount; i++)
         {
-            SpawnTile(_startingTile.GetComponent<Tile>(),false);
+            SpawnTile(_startingTile.GetComponent<Tile>());
+        }
+        //SpawnTile(SelectRandomGameObject(_turnTiles).GetComponent<Tile>());
+        SpawnTile(_turnTiles[0].GetComponent<Tile>());
+        AddNewDirection(Vector3.left);
+    }
+
+    private void SpawnTile(Tile tile, bool spawnObstacle = false)
+    {
+        Quaternion newTileRotation = tile.gameObject.transform.rotation 
+                                     * Quaternion.LookRotation(_currentTileDirection, Vector3.up);
+
+        _prevTile = Instantiate(tile.gameObject, _currentTileLocation, newTileRotation);
+        _currentTiles.Add(_prevTile);
+        if(tile.type == TileType.STRAIGHT)
+            _currentTileLocation += Vector3.Scale(_prevTile.GetComponent<Renderer>().bounds.size,_currentTileDirection);
+    }
+
+    public void AddNewDirection(Vector3 direction)
+    {
+        _currentTileDirection = direction;
+        DeletePreviousTile();
+
+        Vector3 tilePlacementScale;
+        if (_prevTile.GetComponent<Tile>().type == TileType.SIDEWAYS)
+        {
+            tilePlacementScale = Vector3.Scale(
+                _prevTile.GetComponent<Renderer>().bounds.size/2
+                + Vector3.one * _startingTile.GetComponent<BoxCollider>().size.z/2
+                ,_currentTileDirection);
+        }
+        else
+        {
+            tilePlacementScale = Vector3.Scale(
+                (_prevTile.GetComponent<Renderer>().bounds.size - (Vector3.one * 2))
+                + (Vector3.one * _startingTile.GetComponent<BoxCollider>().size.z / 2)
+                , _currentTileDirection
+                );
         }
 
-        
-    }
+        _currentTileLocation += tilePlacementScale;
 
-    private void SpawnTile(Tile tile, bool spawnObstacle)
+        int currentPathLenght = Random.Range(_miniumStraightTile, _maximumStraightTile);
+        for(int i = 0; i < currentPathLenght; i++)
+        {
+            SpawnTile(_startingTile.GetComponent<Tile>(), (i != 0));
+        }
+
+        SpawnTile(SelectRandomGameObject(_turnTiles).GetComponent<Tile>());
+    }
+    private GameObject SelectRandomGameObject(List<GameObject> gameObjects)
     {
-        _prevTile = Instantiate(tile.gameObject, _currentTileLocation, Quaternion.identity);
-        _currentTiles.Add(_prevTile);
-        _currentTileLocation += Vector3.Scale(_prevTile.GetComponent<Renderer>().bounds.size,_currentTileDirection);
-
+        if (gameObjects.Count == 0) return null;
+        return gameObjects[Random.Range(0, gameObjects.Count)];
     }
+    private void DeletePreviousTile()
+    {}
 }
