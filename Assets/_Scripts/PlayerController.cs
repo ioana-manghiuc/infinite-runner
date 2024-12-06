205c90c75b71308a1cf19a890f8c6600825a4768
RobyDoge <robert-ionut.popa@student.unitbv.ro>
2024-12-06 18:57:32 +0200

    Implemented PlayerTurn And Sliding ; TURN NOT WORKING

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,98 +1,201 @@
-using Unity.VisualScripting;
+using System;
+using System.Collections;
+using System.Globalization;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
+using Debug = System.Diagnostics.Debug;
 
-[RequireComponent(typeof(CharacterController),typeof(PlayerInput))]
-public class PlayerController : MonoBehaviour
+
+namespace TempleRun.Player
 {
-    [SerializeField]
-    private float _initialPlayerSpeed = 4f;
-    [SerializeField]
-    private float _maxiumPlayerSpeed = 30f;
-    [SerializeField]
-    private float _playerSpeedIncreaseRate = .1f;
-    [SerializeField]
-    private float _jumpHeight = 1f;
-    [SerializeField]
-    private float _initialGravityValue = -9.8f;
-    [SerializeField]
-    private LayerMask _groundLayer;
-
-    private float _playerSpeed;
-    private float _gravity;
-    private Vector3 _movementDirection = Vector3.forward;
-
-    private PlayerInput _playerInput;
-    private InputAction _turnAction;
-    private InputAction _jumpAction;
-    private InputAction _slideAction;
-
-    private CharacterController _controller;
-
-    private void Awake()
+    [RequireComponent(typeof(CharacterController),typeof(PlayerInput))]
+    public class PlayerController : MonoBehaviour
     {
-        _playerInput = GetComponent<PlayerInput>();
-        _controller = GetComponent<CharacterController>();
-        _turnAction = _playerInput.actions["Turn"];
-        _jumpAction = _playerInput.actions["Jump"];
-        _slideAction = _playerInput.actions["Slide"];
+        [SerializeField]
+        private float _initialPlayerSpeed = 4f;
+        [SerializeField]
+        private float _maxiumPlayerSpeed = 30f;
+        [SerializeField]
+        private float _playerSpeedIncreaseRate = .1f;
+        [SerializeField]
+        private float _jumpHeight = 1f;
+        [SerializeField]
+        private float _initialGravityValue = -9.8f;
+        [SerializeField]
+        private LayerMask _groundLayer;
+        [SerializeField]
+        private LayerMask _turnLayer;
+        [SerializeField]
+        private AnimationClip _slidingAnimationClip;
+        [SerializeField]
+        private Animator _animator;
+
+        private float _playerSpeed;
+        private float _gravity;
+        private Vector3 _movementDirection = Vector3.forward;
+        private Vector3 _playerVelocity;
+
+        private PlayerInput _playerInput;
+        private InputAction _turnAction;
+        private InputAction _jumpAction;
+        private InputAction _slideAction;
+
+        private CharacterController _controller;
+        
+
+        private int _slidingAnimationId;
+
+        private bool _sliding = false;
+
+        [SerializeField]
+        private UnityEvent<Vector3> _turnEvent;
+
+        private void Awake()
+        {
+            _playerInput = GetComponent<PlayerInput>();
+            _controller = GetComponent<CharacterController>();
+            _turnAction = _playerInput.actions["Turn"];
+            _jumpAction = _playerInput.actions["Jump"];
+            _slideAction = _playerInput.actions["Slide"];
+            _slidingAnimationId = Animator.StringToHash("Sliding");
+        }
+        private void Start()
+        {
+            _playerSpeed = _initialPlayerSpeed;
+            _gravity = _initialGravityValue;
+        }
+
+        private void OnEnable()
+        {
+            _turnAction.performed += PlayerTurn;
+            _jumpAction.performed += PlayerJump;
+            _slideAction.performed += PlayerSlide;
+        }
+        private void OnDisable()
+        {
+            _turnAction.performed -= PlayerTurn;
+            _jumpAction.performed -= PlayerJump;
+            _slideAction.performed -= PlayerSlide;
+        }
+        private void Update()
+        {
+            _controller.Move(transform.forward * _playerSpeed * Time.deltaTime);
+
+            if (IsGrounded() && _playerVelocity.y < 0)
+            {
+                _playerVelocity.y = 0f;
+            }
+            _playerVelocity.y += _gravity * Time.deltaTime;
+            _controller.Move(_playerVelocity * Time.deltaTime);
+        }
+
+
+        private void PlayerTurn(InputAction.CallbackContext context)
+        {
+            var turnPosition = CheckTurn(context.ReadValue<float>());
+           
+            if (!turnPosition.HasValue) return;
+
+            var targetDirection = Quaternion.AngleAxis(90 * context.ReadValue<float>(), Vector3.up)
+                * _movementDirection;
+            _turnEvent.Invoke(targetDirection);
+            Turn(context.ReadValue<float>(), turnPosition.Value);
+        }
+
+        private void Turn(float turnValue, Vector3 turnPosition)
+        {
+            var tempPlayerPosition = new Vector3(turnPosition.x, transform.position.y, turnPosition.z);
+            _controller.enabled = false;
+            transform.position = tempPlayerPosition;
+            _controller.enabled = true;
+
+            while (_controller.enabled == false)
+                _controller.enabled = true;
+
+            var test = Quaternion.Euler(0, 90 * turnValue, 0);
+            Quaternion targetRotation = transform.rotation * Quaternion.Euler(0, 90 * turnValue, 0);
+            transform.rotation = targetRotation;
+            _movementDirection = transform.forward.normalized;
+
+        }
+
+        private Vector3? CheckTurn(float turnValue)
+        {
+            
+            var hitColliders = Physics.OverlapSphere(transform.position, .1f, _turnLayer);
+            if (hitColliders.Length != 0)
+            {
+                var tile = hitColliders[0].transform.parent.GetComponent<Tile>();
+                var type = tile.type;
+
+                if ((type == TileType.LEFT && turnValue == -1)
+                    || (type == TileType.RIGHT && turnValue == 1)
+                    || (type == TileType.SIDEWAYS))
+                    return tile.pivot.position;
+            }
+
+            return null;
+        }
+
+        private void PlayerSlide(InputAction.CallbackContext context)
+        {
+            if(!_sliding && IsGrounded())
+                StartCoroutine(Slide());
+        }
+
+        private IEnumerator Slide()
+        {
+            _sliding = true;
+
+            //Shrink the collider
+            Vector3 originalControllerCenter = _controller.center;
+            var newControllerCenter = originalControllerCenter;
+            _controller.height /= 2;
+            newControllerCenter.y -= _controller.height / 2;
+            _controller.center = newControllerCenter;
+
+
+            //Play the sliding animation
+            _animator.Play(_slidingAnimationId);
+            yield return new WaitForSeconds(_slidingAnimationClip.length);
+
+            //retrun the collider to its original size
+            _controller.height *= 2;
+            _controller.center = originalControllerCenter;
+
+            _sliding = false;
+
+        }
+        private void PlayerJump(InputAction.CallbackContext context)
+        {
+            if (!IsGrounded()) return;
+            _playerVelocity.y += Mathf.Sqrt(_jumpHeight * _gravity * -3f);
+            _controller.Move(_playerVelocity * Time.deltaTime);
+        }
+
+
+        private bool IsGrounded(float length = .2f)
+        {
+            Vector3 raycastOriginFirst = transform.position;
+            raycastOriginFirst.y -= _controller.height / 2f;
+            raycastOriginFirst.y += 0.1f;
+
+            Vector3 raycastOriginSecond = raycastOriginFirst;
+            raycastOriginFirst -= transform.forward * .2f;
+            raycastOriginSecond += transform.forward * .2f;
+
+            // Debug.DrawLine(raycastOriginFirst, Vector3.down, Color.green, 2f);
+            // Debug.DrawLine(raycastOriginSecond, Vector3.down, Color.red, 2f);
+
+            if (Physics.Raycast(raycastOriginFirst, Vector3.down, out RaycastHit hit, length, _groundLayer)
+               || Physics.Raycast(raycastOriginSecond, Vector3.down, out RaycastHit hit2, length, _groundLayer))
+                return true;
+
+            return false;
+        }
+
+
     }
-    private void Start()
-    {
-        _playerSpeed = _initialPlayerSpeed;
-        _gravity = _initialGravityValue;
-    }
-
-    private void OnEnable()
-    {
-        _turnAction.performed += PlayerTurn;
-        _jumpAction.performed += PlayerJump;
-        _slideAction.performed += PlayerSlide;
-    }
-    private void OnDisable()
-    {
-        _turnAction.performed -= PlayerTurn;
-        _jumpAction.performed -= PlayerJump;
-        _slideAction.performed -= PlayerSlide;
-    }
-
-    private void PlayerTurn(InputAction.CallbackContext context)
-    {
-    }
-    private void PlayerSlide(InputAction.CallbackContext context)
-    {
-    }
-    private void PlayerJump(InputAction.CallbackContext context)
-    {
-    }
-
-    private void Update()
-    {
-        _controller.Move(transform.forward * _playerSpeed * Time.deltaTime);
-    }
-
-    private bool IsGrounded(float length)
-    {
-        Vector3 raycastOriginFirst = transform.position;
-        raycastOriginFirst.y -= _controller.height / 2f;
-        raycastOriginFirst.y += 0.1f;
-
-        Vector3 raycastOriginSecond = raycastOriginFirst;
-        raycastOriginFirst -= transform.forward * .2f;
-        raycastOriginSecond += transform.forward * .2f;
-
-        // Debug.DrawLine(raycastOriginFirst, Vector3.down, Color.green, 2f);
-        // Debug.DrawLine(raycastOriginSecond, Vector3.down, Color.red, 2f);
-
-        if (Physics.Raycast(raycastOriginFirst, Vector3.down, out RaycastHit hit, length, _groundLayer)
-           || Physics.Raycast(raycastOriginSecond, Vector3.down, out RaycastHit hit2, length, _groundLayer))
-            return true;
-
-        return false;
-
-
-    }
-
-
 }
